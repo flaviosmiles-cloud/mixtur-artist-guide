3aa9999c8301b3e15af1a4466915fba5032c6375
flaviosmiles-cloud <flaviosmiles@gmail.com>
2026-09-15 01:45:18 +0200

    Update venues.js

--- venues.js
+++ venues.js
@@ -1,1 +1,151 @@
-// Mixtur venues
+/* =========================================
+   MIXTUR · VENUES DATABASE
+   ========================================= */
+
+window.MIXTUR_VENUES = {
+
+  "Fabra i Coats": {
+
+    name: "Fabra i Coats",
+
+    fullName:
+      "Fabra i Coats · Fàbrica de Creació",
+
+    category:
+      "Venue",
+
+    description:
+      "Fàbrica de Creació",
+
+    address:
+      "Carrer de Sant Adrià, 20, 08030 Barcelona",
+
+    image:
+      "images/venues/fabra-i-coats.jpg",
+
+    maps:
+      "https://www.google.com/maps/search/?api=1&query=Fabra+i+Coats+Carrer+de+Sant+Adria+20+Barcelona",
+
+
+    /* =====================================
+       NEARBY FOOD
+       ===================================== */
+
+    nearbyFood: [
+
+      {
+        type:
+          "Coffee",
+
+        name:
+          "Lúcido Café",
+
+        description:
+          "Specialty coffee · Breakfast · Pastries",
+
+        address:
+          "Carrer de Camil Fabra, 21, Barcelona",
+
+        maps:
+          "https://www.google.com/maps/search/?api=1&query=Lucido+Cafe+Barcelona"
+      },
+
+      {
+        type:
+          "Restaurant",
+
+        name:
+          "Restaurant l'Andreuenc",
+
+        description:
+          "Catalan cuisine · Lunch",
+
+        address:
+          "Carrer de Neopàtria, 87, Barcelona",
+
+        maps:
+          "https://www.google.com/maps/search/?api=1&query=Restaurant+l%27Andreuenc+Barcelona"
+      },
+
+      {
+        type:
+          "Tapas · Bar",
+
+        name:
+          "La Bodegueta de Sant Andreu",
+
+        description:
+          "Tapas · Drinks · Evening",
+
+        address:
+          "Rambla de Fabra i Puig, 30, Barcelona",
+
+        maps:
+          "https://www.google.com/maps/search/?api=1&query=La+Bodegueta+de+Sant+Andreu+Barcelona"
+      }
+
+    ],
+
+
+    /* =====================================
+       ESSENTIALS
+       ===================================== */
+
+    essentials: [
+
+      {
+        type:
+          "Pharmacy",
+
+        name:
+          "Farmàcia Franquesa",
+
+        description:
+          "Pharmacy",
+
+        address:
+          "Carrer Gran de Sant Andreu, 260, Barcelona",
+
+        maps:
+          "https://www.google.com/maps/search/?api=1&query=Farmacia+Franquesa+Barcelona"
+      },
+
+      {
+        type:
+          "Supermarket",
+
+        name:
+          "Mercadona",
+
+        description:
+          "Food · Drinks · Everyday essentials",
+
+        address:
+          "Carrer de l'Andana de l'Estació, 48, Barcelona",
+
+        maps:
+          "https://www.google.com/maps/search/?api=1&query=Mercadona+Andana+de+l%27Estacio+48+Barcelona"
+      },
+
+      {
+        type:
+          "ATM",
+
+        name:
+          "Santander ATM",
+
+        description:
+          "Cash withdrawal · 24 h",
+
+        address:
+          "Carrer Gran de Sant Andreu, 288, Barcelona",
+
+        maps:
+          "https://www.google.com/maps/search/?api=1&query=Santander+ATM+Gran+de+Sant+Andreu+288+Barcelona"
+      }
+
+    ]
+
+  }
+
+};
